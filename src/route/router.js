ebab7cae7fc8b54700e07d501061b3017668ddfc
Alabi Emmanuel <bobslegend795@gmail.com>
2018-11-20 07:40:56 +0100

    login route done

--- src/route/router.js
+++ src/route/router.js
@@ -1,7 +1,10 @@
-const { register } = require('../controllers/user');
+const { register, login } = require('../controllers/user');
 module.exports = (router) => {
     router.route('/register')
         .post(register);
+
+    router.route('/login')
+        .post(login);
 
     return router;
 };
