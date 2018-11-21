69971d97e9556224a804da0c8b9fa9beaf92f333
Alabi Emmanuel <bobslegend795@gmail.com>
2018-11-21 08:27:46 +0100

    done with edit and delete users routes

--- src/route/router.js
+++ src/route/router.js
@@ -1,7 +1,11 @@
-const { register, login } = require('../controllers/user');
+const { register, login, editUser, deleteUser } = require('../controllers/user');
 module.exports = (router) => {
     router.route('/users')
         .post(register);
+
+    router.route('/users/:id')
+        .put(editUser)
+        .delete(deleteUser);
 
     router.route('/login')
         .post(login);
